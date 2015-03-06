445b8301f36f896bf75728d34ffead19c1e59f80
Yi-wen LIN <bongiovi015@gmail.com>
2015-03-06 08:00:22 +0000

    Add sono
    Signed-off-by: Yi-wen LIN <bongiovi015@gmail.com>

--- 01/bundle/bundle.js
+++ 01/bundle/bundle.js
@@ -54,10 +54,27 @@ var ViewCopy = bongiovi.ViewCopy;
 
 function SceneSound() {
 	bongiovi.Scene.call(this);
+
+	this.frequencies = [];
+	this._initSound();
 }
 
 var p = SceneSound.prototype = new bongiovi.Scene();
 p.constructor = SceneSound;
+
+p._initSound = function() {
+	var that = this;
+	this.sound = Sono.load({
+	    url: ['assets/audio/japan.mp3'],
+	    volume: 0.01,
+	    loop: true,
+	    onComplete: function(sound) {
+	    	console.debug("Sound Loaded");
+	    	that.analyser = sound.effect.analyser(64);
+	    	sound.play();
+	    }
+	});
+};
 
 
 p._initTextures = function() {
@@ -71,10 +88,19 @@ p._initViews = function() {
 
 
 p.render = function() {
+	this._getSoundData();
+
 	GL.setMatrices(this.cameraOtho);
 	GL.rotate(this.rotationFront);
 
 	this._vCopy.render(this._textureBg);
+};
+
+
+p._getSoundData = function() {
+	if(this.analyser) {
+		this.frequencies = this.analyser.getFrequencies();
+	}
 };
 
 
